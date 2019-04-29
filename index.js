db2422394d749f3a74a6ad325801ec4b3aed3372
github username <github email address>
2019-04-28 21:11:43 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,45 @@
+function wakeDog(name, breed) {
+    console.log(`Wake ${name} the ${breed}`)
+    return `Wake ${name} the ${breed}`
+}
+
+function leashDog(name, breed) {
+    console.log(`Leash ${name} the ${breed}`)
+    return `Leash ${name} the ${breed}`
+}
+
+function walkToPark(name, breed) {
+    console.log(`Walk to the park with ${name} the ${breed}`)
+    return `Walk to the park with ${name} the ${breed}`
+}
+
+function throwFrisbee(name, breed) {
+    console.log(`Throw the frisbee for ${name} the ${breed}`)
+    return `Throw the frisbee for ${name} the ${breed}`
+}
+
+function walkHome(name, breed) {
+    console.log(`Walk home with ${name} the ${breed}`)
+    return `Walk home with ${name} the ${breed}`
+}
+
+function unleashDog(name, breed) {
+    console.log(`Unleash ${name} the ${breed}`)
+    return `Unleash ${name} the ${breed}`
+}
+
+
+
+function exerciseDog(name, breed) {
+    let done=[]
+    let routine = [
+        wakeDog(name, breed),
+        leashDog(name, breed),
+        walkToPark(name, breed),
+        throwFrisbee(name, breed),
+        walkHome(name, breed),
+        unleashDog(name, breed)
+    ]
+    routine.forEach(fn => done.push(fn(name, breed)))
+    return done
+}
